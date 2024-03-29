03581c8ee65c310eb9f0285925dd0b5921182419
Zakrok09 <eccyboo@protonmail.com>
2024-03-29 14:12:49 +0100

    test: NFA erroneous input when the automaton is split

--- test/automata/regular/NFA.test.ts
+++ test/automata/regular/NFA.test.ts
@@ -2,15 +2,15 @@ import {Symbol, toChar} from "../../../src";
 import {NFA} from "../../../src/automata/regular/NFA";
 
 describe("NFA acceptance testing", () => {
+    let alphabet:Set<Symbol>;
+    let nfa:NFA;
 
-    it("Should correctly accept a working NFA", () => {
-        const alphabet = new Set<Symbol>();
-
+    beforeEach(() => {
+        alphabet = new Set<Symbol>();
         alphabet.add(toChar('a'))
         alphabet.add(toChar('b'))
 
-        const nfa = new NFA(alphabet, "start", false);
-
+        nfa = new NFA(alphabet, "start", false);
         nfa.addStates("1", "2");
         nfa.addState("end", true);
 
@@ -20,11 +20,23 @@ describe("NFA acceptance testing", () => {
         nfa.addEdge("2", toChar('a'), "2");
         nfa.addEdge("2", toChar('b'), "2");
         nfa.addEdge("2", toChar('a'), "end");
+    })
 
+    it("Should correctly accept a working NFA", () => {
         expect(nfa.runString("a")).toBe(false);
         expect(nfa.runString("aa")).toBe(true);
         expect(nfa.runString("ba")).toBe(false);
         expect(nfa.runString("aabbb")).toBe(false);
         expect(nfa.runString("abbbba")).toBe(true);
+    })
+
+    it("should correctly deal with situations where the graph is split", () => {
+        expect(nfa.removeEdge("2", toChar('a'), "end")).toBe(true);
+
+        expect(nfa.runString("a")).toBe(false);
+        expect(nfa.runString("aa")).toBe(false);
+        expect(nfa.runString("ba")).toBe(false);
+        expect(nfa.runString("aabbb")).toBe(false);
+        expect(nfa.runString("abbbba")).toBe(false);
     })
 })
