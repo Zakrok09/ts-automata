5e981ec49e97aa542b8587952936f2fb0edaa533
Zakrok09 <eccyboo@protonmail.com>
2024-04-03 23:54:02 +0200

    fix: export NFA Builder from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {DFA} from "./automata/regular/DFA";
 import {NFA} from "./automata/regular/NFA";
 import {DFABuilder} from "./automata/util/builders/automata/DFABuilder";
+import {NFABuilder} from "./automata/util/builders/automata/NFABuilder";
 
-export {DFA, NFA, DFABuilder}
+export {DFA, NFA, DFABuilder, NFABuilder}
