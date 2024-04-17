27182eb65ad7fd4fc31deeec82dbf9fe63581892
Zakrok09 <eccyboo@protonmail.com>
2024-04-17 22:22:29 +0200

    chore: migrate to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
 
@@ -10,10 +11,17 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'ts-automata',
-            fileName: 'ts-automata',
-            formats: ['es']
+            fileName: 'index',
+            formats: ['es', "cjs"]
         },
     },
+    plugins: [
+        dts({
+            rollupTypes: true,
+            outDir: 'dist',
+            insertTypesEntry: true,
+        })
+    ],
 
     resolve: {
         alias: [
